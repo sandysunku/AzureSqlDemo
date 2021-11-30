adf7cbdc39f5bb88018129ff06e79da8c9c64449
sandysunku <sandeep.sunku@siemens.com>
2021-12-01 00:13:02 +0530

    removed apiuri

--- AzureSqlXamarinDbDemo/AzureSqlXamarinDbDemo/Constants.cs
+++ AzureSqlXamarinDbDemo/AzureSqlXamarinDbDemo/Constants.cs
@@ -2,6 +2,6 @@
 {
     public static class Constants
     {
-        public static string ApiUri = "https://azurexamarindbdemo.azurewebsites.net/api/purchaseitems/";
+        public static string ApiUri = "https://youazurewebsiteaddress/apiaddress";
     }
 }
